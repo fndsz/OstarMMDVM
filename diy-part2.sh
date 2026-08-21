32adcb675f5a66b184e304f92676b021b5c21283
fndsz <45010248+fndsz@users.noreply.github.com>
2026-08-21 12:12:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,10 +3,10 @@
 # MMDVM-OPENWRT
 
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名称
-sed -i 's/ImmortalWrt/Ostar/g' package/base-files/files/bin/config_generate
+#sed -i 's/ImmortalWrt/Ostar/g' package/base-files/files/bin/config_generate
 
 # 修改默认wifi名称ssid为
-sed -i 's/ssid=ImmortalWrt/ssid=Ostar/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i 's/ssid=ImmortalWrt/ssid=Ostar/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
