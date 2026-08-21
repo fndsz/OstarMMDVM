f979cc6bdc0099d3af7d06bdf1939e53f33d37c3
fndsz <45010248+fndsz@users.noreply.github.com>
2026-08-21 18:46:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,3 +6,4 @@ sed -i "/src-git mmdvm/d" feeds.conf.default echo "src-git mmdvm https://github.
 # 添加 devtools feed（包含 oled-tool, libaruipioled） 
 echo "src-git devtools https://github.com/lazywalker/devtools-feeds" >> feeds.conf.default 
 echo "feeds.conf.default 已更新:"
+cat feeds.conf.default
