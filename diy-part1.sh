53c74d185ef99c7b4c1ee88a8bff14671e86fc46
fndsz <45010248+fndsz@users.noreply.github.com>
2026-08-21 19:03:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,7 +2,8 @@
 # OstarMMDVM diy-part1.sh 
 # 在 Update feeds 之前执行 # 
 # 替换 mmdvm feed 为 fndsz/mmdvm-op（包含核心 MMDVM 包） 
-sed -i "/src-git mmdvm/d" feeds.conf.default echo "src-git mmdvm https://github.com/fndsz/mmdvm-op" >> feeds.conf.default
+sed -i "/src-git mmdvm/d" feeds.conf.default 
+echo "src-git mmdvm https://github.com/fndsz/mmdvm-op" >> feeds.conf.default
 # 添加 devtools feed（包含 oled-tool, libaruipioled） 
 echo "src-git devtools https://github.com/lazywalker/devtools-feeds" >> feeds.conf.default 
 echo "feeds.conf.default 已更新:"
